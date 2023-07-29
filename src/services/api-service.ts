10d326fc793b43a0d04f4730e6d782b04b85e2ef
Nic Murphy <nicrmurphy@live.com>
2023-07-29 10:42:36 -0500

    Add debug log to socket connection

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -5,7 +5,10 @@ import { io } from 'socket.io-client'
  * @returns A socket connection to the server
  */
 export const connectToGameServer = () => {
+  console.log(`Connecting to ${import.meta.env.VITE_WS_URL} via websocket`)
   const socket = io(import.meta.env.VITE_WS_URL)
+  
+  console.log(socket)
   
   socket.on('hello', arg => {
     console.log(arg)
